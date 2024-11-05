ed2cd592d78b7c0903a0a945d85b85e74a476b7a
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2024-11-05 09:18:00 +0100

    Update verlet.h

--- src/verlet.h
+++ src/verlet.h
@@ -1,3 +1,33 @@
 #ifndef __VERLET_H__
 #define __VERLET_H__
-#include "mathc.h"
+#include "math.h"
+
+#define ContainerRadius 6.0f
+#define VerletRadius 0.15f
+
+// Structs
+typedef struct {
+  mfloat_t T[VEC3_SIZE]; // current particle position
+  mfloat_t deltaT[VEC3_SIZE]; // previous particle position
+  mfloat_t a[VEC_SIZE] // particle acceleration
+  mfloat_t radius;
+} ParticleObject;
+
+struct NodeStruct {
+  ParticleObject* val;
+  struct NodeStruct* next;
+};
+
+typedef struct NodeStruct Node;
+
+// Functions
+void updatePos(ParticleObject* object, int size, float dt);
+void force(ParticleObject* object, int size);
+void collision(ParticleObject* object, int size);
+void constraint(ParticleObject* object, int size, mfloat_t* containerPos);
+void clearGrid();
+void fillGrid(VerletObject* objects, int size);
+void applyGridCollisions(VerletObject* objects, int size);
+void addForce(VerletObject* objects, int size, mfloat_t* center, float strength);
+
+#endif
